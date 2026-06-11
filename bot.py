4d35568db28ca94248adbd916b0f7b36486776ce
pdfabaricia-cmd <pdfabaricia@gmail.com>
2026-06-11 17:12:30 -0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,21 +8,9 @@ CHAT_ID = os.getenv("CHAT_ID")
 bot = Bot(token=TOKEN)
 
 async def main():
-    mensaje = """
-🚨 PRUEBA NUEVA 🚨
-
-Si recibes este mensaje,
-GitHub está ejecutando el código correcto.
-
-RTX 5060
-RTX 5070
-
-Hora de prueba
-"""
-
     await bot.send_message(
         chat_id=CHAT_ID,
-        text=mensaje
+        text="🚨 MENSAJE DE PRUEBA 123456 🚨"
     )
 
 asyncio.run(main())
