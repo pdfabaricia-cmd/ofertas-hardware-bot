b0c78b43c85f5af91906338db9ad6f99914b7594
pdfabaricia-cmd <pdfabaricia@gmail.com>
2026-06-11 16:53:55 -0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,9 +8,21 @@ CHAT_ID = os.getenv("CHAT_ID")
 bot = Bot(token=TOKEN)
 
 async def main():
+    mensaje = """
+📊 Hardware monitoreado
+
+1. RTX 5060
+2. RTX 5070
+3. Ryzen 7 9700X
+4. Samsung 990 Pro 2TB
+5. DDR5 32GB
+
+✅ Bot activo
+"""
+    
     await bot.send_message(
         chat_id=CHAT_ID,
-        text="🔥 ¡Tu bot funciona correctamente!"
+        text=mensaje
     )
 
 asyncio.run(main())
